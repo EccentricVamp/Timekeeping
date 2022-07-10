5bd150816bb5a120f8e77426945173345e7a4aaa
EccentricVamp <EccentricVamp@gmail.com>
2022-07-10 11:10:24 -0500

    Override default app

---
+++ src/routes/_app.tsx
@@ -0,0 +1,18 @@
+/** @jsx h */
+import { h } from "preact";
+import { Head } from "$fresh/runtime.ts"
+import { tw } from "@twind";
+import { AppProps } from "$fresh/server.ts";
+
+export default function App({ Component }: AppProps) {
+  return (
+    <html>
+      <Head>
+        <title>Timekeeping</title>
+      </Head>
+      <body class={tw`text-black dark:text-white bg-white dark:bg-black`}>
+        <Component />
+      </body>
+    </html>
+  );
+}
