1eba22f296650e87fed3578c6c95717b367540cf
EccentricVamp <EccentricVamp@gmail.com>
2022-07-10 15:07:45 -0500

    Add colors to Twind and simplify main

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,22 @@
 /// <reference lib="deno.ns" />
 /// <reference lib="deno.unstable" />
 
-import { InnerRenderFunction, RenderContext, start } from "$fresh/server.ts";
-import manifest from "./fresh.gen.ts";
-
-import { setup } from "@twind";
+import { start } from "$fresh/server.ts";
 import { virtualSheet } from "twind/sheets";
+import { setup, theme } from "@twind";
+import routes from "./fresh.gen.ts";
 
 const sheet = virtualSheet();
 sheet.reset();
-setup({ sheet });
+setup({ sheet, theme });
 
-function render(ctx: RenderContext, render: InnerRenderFunction) {
-  const snapshot = ctx.state.get("twind") as unknown[] | null;
-  sheet.reset(snapshot || undefined);
-  render();
-  ctx.styles.splice(0, ctx.styles.length, ...(sheet).target);
-  const newSnapshot = sheet.reset();
-  ctx.state.set("twind", newSnapshot);
-}
-
-await start(manifest, { render });
+await start(routes, {
+  render(ctx, render) {
+    const snapshot = ctx.state.get("twind") as unknown[] | null;
+    sheet.reset(snapshot || undefined);
+    render();
+    ctx.styles.splice(0, ctx.styles.length, ...(sheet).target);
+    const newSnapshot = sheet.reset();
+    ctx.state.set("twind", newSnapshot);
+  },
+});
--- src/utils/twind.ts
+++ src/utils/twind.ts
@@ -1,4 +1,18 @@
+import * as colors from "twind/colors";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { setup } from "twind";
+
+if (IS_BROWSER) setup({ theme: { colors } });
+
+export const theme = {
+  colors: {
+    blue: colors.blue,
+    black: colors.black,
+    gray: colors.gray,
+    green: colors.green,
+    white: colors.white,
+    yellow: colors.yellow,
+    transparent: "transparent",
+  },
+};
 export * from "twind";
-if (IS_BROWSER) setup();
