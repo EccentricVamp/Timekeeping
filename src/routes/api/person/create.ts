e153899031d1af59fc066a5024dbea42644101af
EccentricVamp <EccentricVamp@gmail.com>
2022-07-29 19:52:31 -0500

    Remove unused person API

--- src/routes/api/person/create.ts
+++
@@ -1,9 +0,0 @@
-import { Handlers } from "$fresh/server.ts";
-import { Person } from "@models/person.ts";
-
-export const handler: Handlers = {
-  async POST(req: Request) {
-    await Person.create([await req.json()]);
-    return new Response();
-  },
-};
