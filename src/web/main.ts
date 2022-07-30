9a8b5986147512dc62e7f8b4d220f0013426c083
EccentricVamp <EccentricVamp@gmail.com>
2022-07-30 13:53:19 -0500

    Attempt to fix twind forms preset

--- src/web/main.ts
+++ src/web/main.ts
@@ -1,8 +1,7 @@
 import { start } from "fresh/server.ts";
 
-import { RestoreSnapshot, setup, virtual } from "twind";
+import { RestoreSnapshot, setup } from "twind";
 import presetTailwind from "@twind/preset-tailwind";
-import presetTailwindForms from "@twind/preset-tailwind-forms";
 
 import { database } from "infrastructure/database.ts";
 import { Person } from "domain/models/person.ts";
@@ -11,15 +10,14 @@ import routes from "./fresh.gen.ts";
 
 database.link([Person]);
 
-const sheet = virtual()
-setup({ presets: [presetTailwind(), presetTailwindForms()] }, sheet);
+const tw = setup({ presets: [presetTailwind()] });
 
 await start(routes, {
   render(ctx, innerRender) {
     const restore = ctx.state.get("twind") as RestoreSnapshot | null;
-    if (restore != null) restore();
+    if (restore) restore();
     innerRender();
-    ctx.styles.splice(0, ctx.styles.length, ...sheet.target)
-    ctx.state.set("twind", sheet.snapshot());
+    ctx.styles.splice(0, ctx.styles.length, ...tw.target as string[])
+    ctx.state.set("twind", tw.snapshot());
   }
 });
