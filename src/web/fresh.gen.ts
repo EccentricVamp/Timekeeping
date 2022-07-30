1bc7851bef9e8d20bacf4a002aeea22b88390e5e
EccentricVamp <EccentricVamp@gmail.com>
2022-07-30 14:01:07 -0500

    Update manifest

--- src/web/fresh.gen.ts
+++ src/web/fresh.gen.ts
@@ -3,18 +3,16 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/_app.tsx";
-import * as $1 from "./routes/api/person/create.ts";
-import * as $2 from "./routes/index.tsx";
-import * as $3 from "./routes/person/create.tsx";
-import * as $4 from "./routes/person/index.tsx";
+import * as $1 from "./routes/index.tsx";
+import * as $2 from "./routes/person/create.tsx";
+import * as $3 from "./routes/person/index.tsx";
 
 const manifest = {
   routes: {
     "./routes/_app.tsx": $0,
-    "./routes/api/person/create.ts": $1,
-    "./routes/index.tsx": $2,
-    "./routes/person/create.tsx": $3,
-    "./routes/person/index.tsx": $4,
+    "./routes/index.tsx": $1,
+    "./routes/person/create.tsx": $2,
+    "./routes/person/index.tsx": $3,
   },
   islands: {},
   baseUrl: import.meta.url,
